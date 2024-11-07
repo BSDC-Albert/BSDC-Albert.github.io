df10346fbc330a19138fd63688962a99fc32257b
Albert <30268261@bsdc.ac.uk>
2024-11-07 11:07:33 +0000

    ghfghfghfghfghf

--- Bootstrap/app.js
+++ Bootstrap/app.js
@@ -1,14 +1,1 @@
 console.log("Get out of the console NOW")
-var aTags = document.querySelectorAll('span[data-href]');
-
-for(var i = 0; i < aTags.length; i++){
-    var aTag = aTags[i];
-    aTag.addEventListener('click', function(e){
-        var ele = e.target;
-        window.location.replace(ele.getAttribute('data-href'));
-    });    
-}
-span[data-href]{
-    cursor:pointer;
-}
-<span data-href="http://www.google.com">test</span>
