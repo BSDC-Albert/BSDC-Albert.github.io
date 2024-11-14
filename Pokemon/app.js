53fb3e2ad18a057de1bd7cdb9e8da69f3e8ce3a6
Albert <30268261@bsdc.ac.uk>
2024-11-14 11:57:21 +0000

    hfsfdgffgfggf

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -2,7 +2,12 @@ const container = document.querySelector('#container')
 
 const newImg = document.createElement('img')
 newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/722.png'
-
 container.appendChild(newImg)
 
 console.log(newImg)
+
+for (i=1; 1 <=10; i++) {
+    const newImg = document.createElement('img')
+    newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/722.png'
+    container.appendChild(newImg);
+}
