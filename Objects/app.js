f26cc1558b36293075b2a387d9b928f121478f9e
Albert <30268261@bsdc.ac.uk>
2024-11-20 16:11:07 +0000

    yfyfyyfiygiuygu

---
+++ Objects/app.js
@@ -0,0 +1,13 @@
+const cars = [
+    {type:"Fiat", model:"500", colour:"white"},
+    {type:"BMW", model:"500", colour:"blue"},
+    {type:"BMW", model:"500", colour:"silver"},
+    {type:"BMW", model:"500", colour:"white"},
+    {type:"Fiat", model:"500", colour:"white"},
+    {type:"Fiat", model:"500", colour:"white"},
+    {type:"Fiat", model:"500", colour:"white"},
+    {type:"Fiat", model:"500", colour:"white"},
+    {type:"Fiat", model:"500", colour:"white"},
+];
+const carsSection = document.getElementById('cars');
+console.log(carsSection)
