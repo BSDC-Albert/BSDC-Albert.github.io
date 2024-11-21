8fca411f91bb6fb43d242aefc29d90afdbc04fe6
Albert <30268261@bsdc.ac.uk>
2024-11-21 09:08:54 +0000

    hhhghgdhgd

--- Objects/app.js
+++ Objects/app.js
@@ -11,3 +11,8 @@ const cars = [
 ];
 const carsSection = document.getElementById('cars');
 console.log(carsSection)
+const newCar = document.createElement('tr');
+newCar.innerHTML = `<td>${carList[0].type}</td><td>${carList[0].model}</td><td>${carList[0].colour}/<td>`
+carsTable.appendChild(newcar)
+
+for (i=1;i<carList.length;i++);
