b7d56aaac108cfa73f12246369e123ed563cf5a6
Albert <30268261@bsdc.ac.uk>
2024-11-21 11:08:05 +0000

    jkjkjkknk

--- Objects/app.js
+++ Objects/app.js
@@ -1,18 +1,24 @@
-const cars = [
+const carList = [
     {type:"Fiat", model:"500", colour:"white"},
-    {type:"BMW", model:"500", colour:"blue"},
-    {type:"BMW", model:"500", colour:"silver"},
-    {type:"BMW", model:"500", colour:"white"},
-    {type:"Fiat", model:"500", colour:"white"},
-    {type:"Fiat", model:"500", colour:"white"},
-    {type:"Fiat", model:"500", colour:"white"},
-    {type:"Fiat", model:"500", colour:"white"},
-    {type:"Fiat", model:"500", colour:"white"},
+    {type:"BMW", model:"316", colour:"silver"},
+    {type:"BMW", model:"316", colour:"blue"},
+    {type:"Ford", model:"ka", colour:"white"},
+    {type:"Audi", model:"A4", colour:"red"},
+    {type:"Renault", model:"Clio", colour:"red"},
+    {type:"Peugeot", model:"Expert", colour:"blue"},
+    {type:"Skoda", model:"Octavia", colour:"red"},
 ];
-const carsSection = document.getElementById('cars');
-console.log(carsSection)
-const newCar = document.createElement('tr');
-newCar.innerHTML = `<td>${carList[0].type}</td><td>${carList[0].model}</td><td>${carList[0].colour}/<td>`
-carsTable.appendChild(newcar)
+const carsTable = document.getElementById('carsTable');
+console.log(carsTable.innerHTML);
+for (i=0;i<carList.length;i++) {
+    const newCar = document.createElement('tr');
+    newCar.innerHTML = `
+    <td>${carList[i].type}</td>
+    <td>${carList[i].model}</td>
+    <td>${carList[i].colour}</td>
+    <button>button</button>`;
 
-for (i=1;i<carList.length;i++);
+    console.log(newCar)
+    
+    carsTable.appendChild(newCar);
+    };
